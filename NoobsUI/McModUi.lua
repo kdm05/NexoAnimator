854f1d592bac5b01e5050aebda008d25b014364a
Nathan <46389108+IAmMrNoob@users.noreply.github.com>
2022-05-16 19:28:54 -0400

    Update McModUi.lua

--- NoobsUI/McModUi.lua
+++ NoobsUI/McModUi.lua
@@ -1,43 +1,47 @@
 local e = {}
 
 function e:Ui(name,CanDrag)
-	name = name or ''
-	CanDrag = CanDrag or false
-	local f = Instance.new("ScreenGui",game.CoreGui)
+	local UIPARENT = game.CoreGui
+	if game:GetService("RunService"):IsStudio() then
+		UIPARENT = game.Players.LocalPlayer.PlayerGui
+	end
+	local f = Instance.new("ScreenGui",UIPARENT)
 	f.Name = name
 	local modal = Instance.new('TextButton',f)
 	modal.Size = UDim2.new(0,0,0,0)
-	modal.Text = ''
 	local screen = {}
 	local lol = {}
 	local windowsCreated = -1
 	local plr = game.Players.LocalPlayer
-	
+
 	function lol:SetEnabled(bool)
 		f.Enabled = bool
 	end
-	
+
 	function lol:GetEnabled()
 		return f.Enabled
 	end
-	
+
 	function lol:Player()
 		return plr
 	end
-	
+
 	function lol:MouseState(bool)
 		modal.Modal = bool
 	end
-	
+
 	function lol.newWindow(TitleText,color3,rotation,EnumFont)
-		TitleText = TitleText or ''
-		color3 = color3 or Color3.new(1, 1, 1)
-		rotation = TitleText or 0
+		if typeof(TitleText) == "table" then
+			TitleText = nil
+		end
+		TitleText = TitleText or "TitleText"
+		color3 = color3 or nil
+		rotation = rotation or 0
 		local b = {}
 		local offsetwindow = {
 			X = 0
 		}
-			windowsCreated = windowsCreated + 1
+		windowsCreated = windowsCreated + 1
 		if windowsCreated == 7 then
 			windowsCreated = -1
 			windowsCreated = windowsCreated + 1
@@ -54,6 +58,7 @@ function e:Ui(name,CanDrag)
 		local textsize = 13
 		local padding1 = 34
 		local padding2 = 104
+		local padding3 = 52
 		local screenincre =0
 		if #screen > 7 then
 			screenincre = #screen - 7
@@ -61,13 +66,13 @@ function e:Ui(name,CanDrag)
 		else
 			s.Position = UDim2.new(0, 50+(210*windowsCreated),0, 50)
 		end
-		
+
 		s.Name = TitleText
 		s.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 		s.ClipsDescendants = true
 		s.Size = UDim2.new(0, 200, 0, 27)
-		
-		
+
+
 		if CanDrag ~= nil and CanDrag then --stolen from someone plugin on rblxst lol didnt want to use .draggable XD
 			local UserInputService = game:GetService("UserInputService")
 
@@ -129,14 +134,12 @@ function e:Ui(name,CanDrag)
 
 		if color3 ~= nil then
 			if typeof(color3) == "Color3" then
-				s5.Color = color3
+				s5.Color = ColorSequence.new(color3)
 			else
-				if typeof(color3) == "ColorSequenceKeypoint" then
-					s5.Color = color3.Value
-				elseif typeof(color3) == "table" then
+				if typeof(color3[1]) == "Color3" then
+					s5.Color = ColorSequence.new(color3[1],color3[2])
+				else
 					s5.Color = ColorSequence.new(color3)
-				elseif typeof(color3) == "ColorSequence" then
-					s5.Color = color3
 				end
 			end
 		end
@@ -178,7 +181,7 @@ function e:Ui(name,CanDrag)
 					end)
 				end
 			end
-			
+
 			s.Size = s.Size + UDim2.new(0,0,0,padding1)
 
 			return r
@@ -275,7 +278,7 @@ function e:Ui(name,CanDrag)
 					OnPressed()
 				end
 			end)
-			
+
 			s.Size = s.Size + UDim2.new(0,0,0,padding1)
 
 			return c
@@ -294,12 +297,12 @@ function e:Ui(name,CanDrag)
 			local hlt = Instance.new("TextLabel",hlf)
 			local htuc = Instance.new("UICorner",hlt)
 			SwitchText = SwitchText or "Switch"
-			
+
 			local bruh = {}
 			local state = defualtState or false
 			local oCFunct
 			OnChange = OnChange or function() end
-			
+
 			g.Name = SwitchText
 			g.Parent = s
 			g.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
@@ -325,7 +328,7 @@ function e:Ui(name,CanDrag)
 			sw.LayoutOrder = 1
 			sw.Position = UDim2.new(0.59, 0, 0, 0)
 			sw.Size = UDim2.new(.45, 0, 1, 0)
-			
+
 			sw.ImageTransparency = 1
 
 			hlf.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
@@ -343,7 +346,7 @@ function e:Ui(name,CanDrag)
 			hlt.TextColor3 = Color3.fromRGB(0, 0, 0)
 			hlt.TextSize = textsize
 			local db = false
-			
+
 			oCFunct = sw.MouseButton1Click:Connect(function()
 				if db == true then return end
 				db = true
@@ -374,12 +377,12 @@ function e:Ui(name,CanDrag)
 					end)
 				end
 			end
-			
+
 			s.Size = s.Size + UDim2.new(0,0,0,padding1)
-			
+
 			return bruh
 		end
-		
+
 		function b:Text(Text)
 			local v = Instance.new("TextLabel",s)
 			local d = Instance.new("UICorner",v)
@@ -395,16 +398,16 @@ function e:Ui(name,CanDrag)
 			v.Text = Text
 			v.TextColor3 = Color3.fromRGB(0, 0, 0)
 			v.TextSize = textsize
-			
+
 			function c:newText(nText)
 				v.Text = tostring(nText)
 			end
-			
+
 			s.Size = s.Size + UDim2.new(0,0,0,padding1)
-			
+
 			return c
 		end
-		
+
 		function b:TextBox(Name,Text,Hint,COF,OnChanged)
 			local c = Instance.new("Frame",s)
 			local d = Instance.new("UICorner",c)
@@ -415,14 +418,14 @@ function e:Ui(name,CanDrag)
 			local i = Instance.new("UICorner",h)
 			local j = {}
 			local k 
-			
+
 			Name = Name or "TextBox"
 			Text = Text or "Text"
 			Hint = Hint or "Text"
 			COF = COF or false
-			
+
 			OnChanged = OnChanged or function() end
-			
+
 			c.Name = Name
 			c.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 			c.BackgroundTransparency = .9
@@ -477,12 +480,12 @@ function e:Ui(name,CanDrag)
 					func(Change)
 				end)
 			end
-			
+
 			s.Size = s.Size + UDim2.new(0,0,0,padding1)
-			
+
 			return j
 		end
-		
+
 		function b:Selector(List,Current,onSelected,onChange)
 			s.Size = s.Size + UDim2.new(0,0,0,padding1)
 			local c = {}
@@ -490,6 +493,7 @@ function e:Ui(name,CanDrag)
 			local cc
 			local ListsOBJ = {}
 			
+			List = List or {}
 			Current = Current or 1
 			onSelected = onSelected or function() end
 			onChange = onChange or function() end
@@ -553,7 +557,7 @@ function e:Ui(name,CanDrag)
 			g.LayoutOrder = 2
 			g.TextColor3 = Color3.fromRGB(0, 0, 0)
 			g.TextSize = textsize
-			
+
 			for i,v in ipairs(List) do
 				if v ~= nil then
 					local selection = Instance.new("TextLabel",f)
@@ -567,7 +571,7 @@ function e:Ui(name,CanDrag)
 					table.insert(ListsOBJ,selection)
 				end
 			end
-			
+
 			if Current ~= nil or Current == 0 then
 				if typeof(Current) == "number" then
 					fuip:JumpToIndex(Current)
@@ -589,11 +593,11 @@ function e:Ui(name,CanDrag)
 			g.MouseButton1Click:Connect(function()
 				fuip:Next()
 			end)
-			
+
 			cs = fuip.Stopped:Connect(function(curpa)
 				onSelected(tostring(curpa))
 			end)
-			
+
 			cc = fuip.Changed:Connect(function(Change)
 				onChange(Change)
 			end)
@@ -620,7 +624,7 @@ function e:Ui(name,CanDrag)
 					func(tostring(curpa))
 				end)
 			end
-			
+
 			function c:newOnChanged(func)
 				cc:Disconnect()
 				cc = fuip.Changed:Connect(function(Change)
@@ -630,7 +634,7 @@ function e:Ui(name,CanDrag)
 
 			return c
 		end
-		
+
 		function b:LargeTextBox(Name,Text,Hint,COF,OnChanged)
 			local c = {}
 			local g
@@ -642,13 +646,13 @@ function e:Ui(name,CanDrag)
 			local eu = Instance.new("UICorner",e)
 			local f = Instance.new("TextBox",d)
 			local fu = Instance.new("UICorner",f)
-			
+
 
 			Name = Name or "LargeTextBox"
 			Text = Text or "Text"
 			Hint = Hint or "Text"
 			COF = COF or false
-			
+
 			OnChanged = OnChanged or function() end
 
 			d.Name = Name
@@ -706,15 +710,197 @@ function e:Ui(name,CanDrag)
 					func(Change)
 				end)
 			end
-			
+
 			s.Size = s.Size + UDim2.new(0,0,0,padding2)
 
 			return c
 		end
-		
+
+		function b:Slider(Name,MaxValue,CurrentValue,OnSeek)
+			Name = Name or "Slider"
+			MaxValue = MaxValue or 100
+			CurrentValue = CurrentValue or 0
+			OnSeek = OnSeek or function() end
+			local X = {}
+
+			local a = Instance.new("Frame",s)
+			a.Name = Name
+			a.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+			a.BackgroundTransparency = .9
+			a.Position = UDim2.new(.1, 0, .08, 0)
+			a.Size = UDim2.new(.8, 0, 0, 50)
+			local UICorner = Instance.new("UICorner",a)
+			local UIListLayout = Instance.new("UIListLayout",a)
+			UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+			local b = Instance.new("Frame",a)
+			b.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+			b.BackgroundTransparency = 1
+			b.Size = UDim2.new(1, 0, .5, 0)
+			local c = Instance.new("TextLabel",b)
+			c.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+			c.BackgroundTransparency = 1
+			c.Position = UDim2.new(1.22, 0, 2.28, 0)
+			c.Size = UDim2.new(.75, 0, 1, 0)
+			c.Font = EnumFont
+			c.Text = Name
+			c.TextColor3 = Color3.fromRGB(0, 0, 0)
+			c.TextSize = 14
+			local d = Instance.new("TextBox",b)
+			d.BackgroundColor3 = Color3.fromRGB(90, 90, 90)
+			d.BackgroundTransparency = .9
+			d.Size = UDim2.new(0.25, 0, 1, 0)
+			d.Font = EnumFont
+			d.Text = ''
+			d.LayoutOrder = 1
+			d.TextColor3 = Color3.fromRGB(0, 0, 0)
+			d.TextSize = 14
+			local UICorner_2 = Instance.new("UICorner",d)
+			local UIListLayout_2 = Instance.new("UIListLayout",b)
+			UIListLayout_2.FillDirection = Enum.FillDirection.Horizontal
+			UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
+			local e = Instance.new("Frame",a)
+			e.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+			e.BackgroundTransparency = 1
+			e.Size = UDim2.new(1, 0, .5, 0)
+			local UIListLayout_3 = Instance.new("UIListLayout",e)
+			UIListLayout_3.FillDirection = Enum.FillDirection.Horizontal
+			UIListLayout_3.SortOrder = Enum.SortOrder.LayoutOrder
+			local f = Instance.new("Frame",e)
+			f.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+			f.BackgroundTransparency = .9
+			f.BorderColor3 = Color3.fromRGB(27, 42, 53)
+			f.Size = UDim2.new(1, 0, 1, 0)
+			local UICorner_3 = Instance.new("UICorner",f)
+			local UIListLayout_4 = Instance.new("UIListLayout",f)
+			UIListLayout_4.FillDirection = Enum.FillDirection.Horizontal
+			UIListLayout_4.HorizontalAlignment = Enum.HorizontalAlignment.Center
+			UIListLayout_4.SortOrder = Enum.SortOrder.LayoutOrder
+			UIListLayout_4.VerticalAlignment = Enum.VerticalAlignment.Center
+			local g = Instance.new("ImageButton",f)
+			g.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+			g.BackgroundTransparency = .9
+			g.Size = UDim2.new(.9, 0, .62, 0)
+			g.Image = "rbxasset://textures/ui/GuiImagePlaceholder.png"
+			g.ImageTransparency = 1
+			local UICorner_4 = Instance.new("UICorner",g)
+			local h = Instance.new("Frame",g)
+			h.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+			h.BackgroundTransparency = .7
+			h.BorderColor3 = Color3.fromRGB(27, 42, 53)
+			h.ClipsDescendants = true
+			local UICorner_5 = Instance.new("UICorner",h)
+
+			local uis = game:GetService("UserInputService")
+			local editbox = d
+			local sliderbar = h
+			local isdragg = false
+			local max = MaxValue
+			local min = 0
+			local current = CurrentValue
+			local gui = g
+			local movinggui = sliderbar
+			local dragging
+			local dragInput
+			local dragStart
+			local startPos
+			movinggui.Size = UDim2.new(current/max,0, 1,0)
+
+			editbox.FocusLost:Connect(function()
+				local allowed = {
+					[""] = true,
+					["-"] = true
+				}
+				local text = editbox.Text
+
+				if typeof(tonumber(text)) == nil then
+					editbox.Text = text:sub(1, #text - 1)
+				elseif typeof(tonumber(text)) == "number" then
+					current = tonumber(text)
+					movinggui.Size = UDim2.new(current/max,0, 1,0)
+					OnSeek(current)
+				end
+			end)
+
+			local function update(input)
+				local delta = input.Position - dragStart
+				wait(.01)
+				if startPos.X.Scale + delta.X/150 > 0 then
+					if startPos.X.Scale + delta.X/150 < 1 then
+						movinggui.Size = UDim2.new(startPos.X.Scale + delta.X/150,0, 1,0)
+					else
+						movinggui.Size = UDim2.new(1,0, 1,0)
+					end
+				else
+					movinggui.Size = UDim2.new(0, 0, 1, 0)
+				end
+				if min > 0 then
+					local a = max-min
+					current = a+(movinggui.Size.X.Scale*min)
+				elseif min == 0 then
+					current = movinggui.Size.X.Scale*max
+				elseif min < 0 then
+					local a = max+min
+					current = a-(movinggui.Size.X.Scale*min)
+				end
+				editbox.Text = math.round(current)
+				OnSeek(current)
+			end
+
+			gui.InputBegan:Connect(function(input)
+				if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+					dragging = true
+					dragStart = input.Position
+					startPos = movinggui.Size
+
+					input.Changed:Connect(function()
+						if input.UserInputState == Enum.UserInputState.End then
+							dragging = false
+						end
+					end)
+				end
+			end)
+
+			gui.InputChanged:Connect(function(input)
+				if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+					dragInput = input
+				end
+			end)
+
+			uis.InputChanged:Connect(function(input)
+				if input == dragInput and dragging then
+					update(input)
+				end
+			end)
+
+			function X:setText(nText)
+				c.Text = tostring(nText)
+			end
+
+			function X:setcurrentValue(Value)
+				current = Value
+				movinggui.Size = UDim2.new(Value/max,0, 1,0)
+			end
+			
+			function X:getcurrentValue()
+				return current
+			end
+			
+			function X:setmaxValue(Value)
+				max = Value
+			end
+
+			function X:newOnSeek(func)
+				OnSeek = func
+			end
+			
+			s.Size = s.Size + UDim2.new(0,0,0,padding3)
+			
+			return X
+		end
+
 		return b
 	end
-	
+
 	return lol
 end
 
